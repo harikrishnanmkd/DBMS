df3ac133cc85ebafad6a8595db75ca764dfa9809
harikrishnanmkd <116252215+harikrishnanmkd@users.noreply.github.com>
2023-07-22 12:48:37 +0530

    Add files via upload

---
+++ Project/welcome.blade.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<link rel="stylesheet"
+		href=
+"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<style>
+		.input-icons i {
+			position: absolute;
+		}
+		
+		.input-icons {
+			width: 100%;
+			margin-bottom: 10px;
+		}
+		
+		.icon {
+			padding: 10px;
+			color: green;
+			min-width: 50px;
+			text-align: center;
+		}
+		
+		.input-field {
+			width: 75%;
+			padding: 5px;
+			text-align: center;
+		}
+		
+		h2 {
+			color: green;
+		}
+                    .button{
+                        text-align:center;
+}
+                       
+                .button:hover {
+                   background-color: yellow; /* Green */
+                    color: black;}
+                    
+	</style>
+</head>
+
+<body>
+	<center>
+		<form style="max-width:450px;margin:auto">
+			<h2><u>	HARI MAMMANTE KADA</u></h2>
+
+			<div class="input-icons">
+				<i class="fa fa-envelope icon">
+			</i>
+				<input class="input-field"
+					type="text"
+					placeholder="Email">
+			</div>
+
+			<div class="input-icons">
+				<i class="fa fa-key icon">
+			</i>
+				<input class="input-field"
+					type="password"
+					placeholder="Password">
+			</div>
+                  <div>
+                     <input class="button"
+                           type="LOGIN"
+                           placeholder="LOGIN"></div>
+		</form>
+	</center>
+</body>
+
+</html>
+
